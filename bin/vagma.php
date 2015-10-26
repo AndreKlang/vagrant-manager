24fc45dbfa1d104903dbc5cddb83b2ce2de24891
Andre Klang <hej@andreklang.se>
2015-10-26 17:50:07 +0100

    Renamed skeleton

--- bin/vagma.php
+++ bin/vagma.php
@@ -5,6 +5,6 @@ require __DIR__.'/../vendor/autoload.php';
 
 use App\Command;
 
-$application = new App('Name of your App', '@package_version@');
+$application = new App('Vagrant Manager', '@package_version@');
 $application->add(new Command\HelloCommand());
 $application->run();
---
+++ src/App/Service/Vagrant.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Service;
+
+use App\Service;
+
+class Vagrant extends Service {
+}
