b12ce6343a558ee766288edafc984a98342bd134
Andre Klang <hej@andreklang.se>
2015-10-30 20:33:13 +0100

    removed jsawk

--- publish-version.sh
+++ publish-version.sh
@@ -10,14 +10,6 @@ fi
 TAG=$1
 
 #
-# get jsawk
-#
-if [ ! -f jsawk ]; then
-    curl -L http://github.com/micha/jsawk/raw/master/jsawk > jsawk
-    chmod +x jsawk
-fi
-
-#
 # Tag & build master branch
 #
 git checkout master
@@ -35,16 +27,15 @@ git add downloads/vagma-${TAG}
 
 SHA1=$(openssl sha1 vagma)
 
-JSON='name:"vagma"'
-JSON="${JSON},sha1:\"${SHA1}\""
-JSON="${JSON},url:\"https://github.com/AndreKlang/vagrant-manager/downloads/vagma-${TAG}\""
-JSON="${JSON},version:\"${TAG}\""
+JSON='"name":"vagma"'
+JSON="${JSON},\"sha1\":\"${SHA1}\""
+JSON="${JSON},\"url\":\"https://github.com/AndreKlang/vagrant-manager/downloads/vagma-${TAG}\""
+JSON="${JSON},\"version\":\"${TAG}\""
 
 #
 # Update manifest
 #
-cat manifest.json | ./jsawk -a "this.push({${JSON}})" | python -mjson.tool > manifest.json.tmp
-mv manifest.json.tmp manifest.json
+php updateManifest.php manifest.json '{'$JSON'}'
 git add manifest.json
 
 git commit -m "Publish version ${TAG}"
---
+++ updateManifest.php
@@ -0,0 +1,11 @@
+<?php
+
+
+$file = $argv[1];
+$add = $argv[2];
+
+$json = json_decode(file_get_contents($file));
+$json[] = json_decode($add);
+
+print_r($json);
+file_put_contents($file, json_encode($json,JSON_PRETTY_PRINT + JSON_UNESCAPED_SLASHES));
