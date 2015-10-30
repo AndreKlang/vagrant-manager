4ae50027eedc9b6f676fd13812eae52c3dd9e29a
Andre Klang <hej@andreklang.se>
2015-10-30 21:00:24 +0100

    fixed publish, at last, I think!

--- publish-version.sh
+++ publish-version.sh
@@ -9,16 +9,20 @@ fi
 
 TAG=$1
 
+mkdir temp-repo
+cp -r .git temp-repo/
+
 #
 # Tag & build master branch
 #
-git checkout master
 git tag ${TAG}
 ./build.sh
+cp vagma temp-repo/
 
 #
 # Copy executable file into GH pages
 #
+cd temp-repo
 git checkout gh-pages
 
 mkdir -p downloads
@@ -26,25 +30,32 @@ cp vagma downloads/vagma-${TAG}
 git add downloads/vagma-${TAG}
 
 SHA1=$(openssl sha1 vagma)
+IFS=', ' read -r -a sha <<< "$SHA1"
 
 JSON='"name":"vagma"'
-JSON="${JSON},\"sha1\":\"${SHA1}\""
+JSON="${JSON},\"sha1\":\"${sha[1]}\""
 JSON="${JSON},\"url\":\"https://github.com/AndreKlang/vagrant-manager/downloads/vagma-${TAG}\""
 JSON="${JSON},\"version\":\"${TAG}\""
 
 #
 # Update manifest
 #
-php updateManifest.php manifest.json '{'$JSON'}'
+php ../updateManifest.php manifest.json '{'$JSON'}'
 git add manifest.json
 
 git commit -m "Publish version ${TAG}"
 
-#
-# Go back to master
-#
-git checkout master
+# push pages
+git push origin gh-pages
 
-echo "New version created. Now you should run:"
-echo "git push origin gh-pages"
-echo "git push ${TAG}"
+# go back
+cd ..
+
+# push the tag
+git push origin refs/tags/${TAG}
+
+# pull the changes
+git pull
+
+# remove the temp-repo
+rm -fr temp-tepo
